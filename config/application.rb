ccf9934a941f88c3f6d43d31b1eb5699bde833a7
mayu nishikawa <sheseesseaontheseashore@gmail.com>
2022-08-08 17:53:52 +0900

    :wrench: generate時の生成ファイル設定

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,14 @@ module CallMe
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local
 
+    config.generators do |g|
+      g.assets false
+      g.helper false
+      g.stylesheets false
+      g.javascripts true
+      g.test_framework false
+    end
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
