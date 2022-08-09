96a1564e5f7babde83a333110b88c8a53149e765
mayu nishikawa <sheseesseaontheseashore@gmail.com>
2022-08-10 00:49:53 +0900

    :fire: メール認証機能の削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,6 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-          :recoverable, :rememberable, :trackable, :validatable, :confirmable
+          :recoverable, :rememberable, :trackable, :validatable
   mount_uploader :icon, UsersIconUploader
 end
