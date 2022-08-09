2893fc798ead926e91647b921d044bf03cf46b0f
mayu nishikawa <sheseesseaontheseashore@gmail.com>
2022-08-09 11:16:40 +0900

    :cake: home画面, controller生成

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  get 'rooms/show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 end
