28040ad2a741513a3ae0ea3cff770005616323d1
mayu nishikawa <sheseesseaontheseashore@gmail.com>
2022-08-12 14:55:15 +0900

    :sparkles: マイページ作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'user/show'
   devise_for :users, controllers: {registrations: 'users/registrations'}
   root 'homes#show'
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
