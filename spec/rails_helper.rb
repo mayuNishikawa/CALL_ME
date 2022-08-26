8977914b1d3a4988f4b3f147b55919f5d6be27e5
mayu nishikawa <sheseesseaontheseashore@gmail.com>
2022-08-26 17:34:22 +0900

    :white_check_mark: user関連テスト

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -15,5 +15,7 @@ RSpec.configure do |config|
   config.infer_spec_type_from_file_location!
 
   config.filter_rails_from_backtrace!
+
   config.include Devise::Test::IntegrationHelpers, type: :system
+  config.include Warden::Test::Helpers
 end
