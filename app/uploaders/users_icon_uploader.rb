3d43339e09212a3b25ee1d5b190b9d5e0d16554b
mayu nishikawa <sheseesseaontheseashore@gmail.com>
2022-08-26 12:31:07 +0900

    :white_check_mark: post_spec

--- app/uploaders/users_icon_uploader.rb
+++ app/uploaders/users_icon_uploader.rb
@@ -8,6 +8,10 @@ class UsersIconUploader < CarrierWave::Uploader::Base
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
+  version :thumb do
+    process resize_to_fill: [50, 50]
+  end
+
   def extension_allowlist
     %w(jpg jpeg gif png)
   end
@@ -15,6 +19,4 @@ class UsersIconUploader < CarrierWave::Uploader::Base
   def default_url(*args)
     ActionController::Base.helpers.asset_path("/images/fallback/" + [thumb, "default_icon.png"].compact.join('_'))
   end
-
-    process resize_to_fill: [50, 50]
 end
---
+++ spec/system/post_spec.rb
@@ -0,0 +1,75 @@
+require 'rails_helper'
+RSpec.describe 'Posting in Lounge', type: :system do
+  let!(:user){ FactoryBot.create(:user) }
+  let!(:post){ FactoryBot.create(:post, user: user) }
+  before do
+    sign_in(user)
+  end
+
+  describe 'the function to create and edit post' do
+    context 'to create new post' do
+      it 'is shown in index page' do
+        visit new_post_path
+        fill_in 'post[content]', with: 'new post'
+        click_on '投稿'
+        expect(page).to have_content 'new post'
+      end
+    end
+
+    context 'to edit the post' do
+      it 'is shown the updated post' do
+        visit edit_post_path(post.id)
+        fill_in 'post[content]', with: 'changed content'
+        click_on '投稿'
+        expect(page).to have_content 'changed content'
+      end
+    end
+  end
+
+  describe 'the function to display in Lounge' do
+    context 'to display task' do
+      it 'is shown in index page' do
+        visit posts_path
+        expect(page).to have_content 'only have content'
+      end
+    end
+
+    context 'to display the new post' do
+      it 'is shown the new post' do
+        visit new_post_path
+        fill_in 'post[content]', with: 'new post'
+        click_on '投稿'
+        visit posts_path
+        expect(page).to have_content 'new post'
+      end
+    end
+
+    context 'to display the updated post' do
+      it 'is shown the updated post' do
+        visit edit_post_path(post)
+        fill_in 'post[content]', with: 'changed content'
+        click_on '投稿'
+        visit posts_path
+        expect(page).to have_content 'changed content'
+      end
+    end
+  end
+  
+  describe 'the function to visit link the post contains' do
+    context 'to click the link to show' do
+      it 'is shown the post\'s page' do
+        visit posts_path
+        click_on 'Show'
+        expect(page).to have_content 'コメント'
+      end
+    end
+
+    context 'to click the icon in the post' do
+      it 'is shown the post\'s page' do
+        visit posts_path
+        click_on "#{user.id}"
+        expect(page).to have_content "#{user.nickname}の部屋"
+      end
+    end
+  end
+end
