b7ec340626373d17651194d882420ce467ec65c8
mayu nishikawa <sheseesseaontheseashore@gmail.com>
2022-08-29 18:32:35 +0900

    :lipsticks: デフォルトアイコンの変更

--- app/javascript/channels/room_channel.js
+++ app/javascript/channels/room_channel.js
@@ -28,42 +28,4 @@ document.addEventListener('turbolinks:load', () => {
     window.scroll(0, documentElement.scrollHeight);
   }
   scrollToBottom();
-
-  // chatContent.addEventListener('input', function(){
-  //   changeLineCheck();
-  // })
-
-  // const maxLineCount = 10;
-
-  // const getLineCount = function(){
-  //     return (chatContent.value + '\n').match(/\r?\n/g).length;
-  // }
-
-  // let lineCount = getLineCount();
-  // let newLineCount;
-
-  // const changeLineCheck = function(){
-  //   newLineCount = Math.min(getLineCount(), maxLineCount);
-  //   if (lineCount !== newLineCount) {
-  //     changeLineCount(newLineCount);
-  //   }
-  // };
-
-  // const footer = document.getElementsByClassName('chat-box');
-  // let footerHeight = footer.scrollHeight;
-  // let newFooterHeight, footerHeightDiff;
-
-  // const changeLineCount = function(newLineCount){
-  //   chatContent.rows = lineCount = newLineCount;
-  //   newFooterHeight = footer.scrollHeight;
-  //   footerHeightDiff = newFooterHeight - footerHeight;
-  //   if (footerHeightDiff > 0) {
-  //     chatContainer.style.paddingBottom = newFooterHeight + 'px';
-  //     window.scrollBy(0, footerHeightDiff);
-  //   } else {
-  //     window.scrollBy(0, footerHeightDiff);
-  //     chatContainer.style.paddingBottom = newFooterHeight + 'px';
-  //   }
-  //   footerHeight = newFooterHeight;
-  // };
 });
