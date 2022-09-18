e6997d6897b5d6da3bfd9ba147a325d419f2f3cc
mayu nishikawa <sheseesseaontheseashore@gmail.com>
2022-09-18 19:36:38 +0900

    :recycle: 可読性とセキュリティの向上

--- app/controllers/assigns_controller.rb
+++ app/controllers/assigns_controller.rb
@@ -11,23 +11,27 @@ class AssignsController < ApplicationController
   end
 
   def destroy
-    assign = Assign.find_by(team_id: params[:id], user_id: current_user)
+    assign = find_user(params[:id])
     assign.destroy
     redirect_to teams_url
   end
 
   private
 
-  def assign_params
-    params[:post][:email]
+  def find_team(*)
+    Team.find(params[:team_id])
   end
 
   def email_reliable?(email)
     email.match(/\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i)
   end
 
-  def find_team(*)
-    Team.find(params[:team_id])
+  def assign_params
+    params[:post][:email]
+  end
+
+  def find_user(*)
+    Assign.find_by(team_id: params[:id], user_id: current_user)
   end
 end
 
